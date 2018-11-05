c0693ad71f8853ad440dd667531eba1c8f13ed23
Emilio Platzer <emilioplatzer@gmail.com>
2018-11-04 23:01:07 -0300

    /ver para recolectar datos del navegador

--- src/server/app-instrumentacion.ts
+++ src/server/app-instrumentacion.ts
@@ -3,14 +3,16 @@
 import * as backendPlus from "backend-plus";
 import {defConfig} from "./def-config";
 import {procedures} from "./procedures-instrumentacion";
-import { AppBackend, ClientModuleDefinition, ProcedureDef, Context, Request, TableContext, TableDefinition, TableDefinitionFunction } from "./types-instrumentacion";
+import { AppBackend, ClientModuleDefinition, ProcedureDef, ExpressPlus, Context, Request, Response, TableContext, TableDefinition, TableDefinitionFunction } from "./types-instrumentacion";
 export * from "./types-instrumentacion";
 
 import { usuarios } from './table-usuarios';
 import { ubicaciones } from './table-ubicaciones';
 import { ip } from './table-ip';
 import { user_agents } from './table-user_agents';
-
+import { html } from 'js-to-html';
+import * as MiniTools from 'mini-tools';
+import { NextFunction } from "express";
 
 export type Constructor<T> = new(...args: any[]) => T;
 export function emergeAppInstrumentacion<T extends Constructor<AppBackend>>(Base:T){
@@ -33,6 +35,37 @@ export function emergeAppInstrumentacion<T extends Constructor<AppBackend>>(Base
         configStaticConfig(){
             super.configStaticConfig();
             this.setStaticConfig(defConfig);
+        }
+
+        addUnloggedServices(mainApp:ExpressPlus, baseUrl:string):void{
+            var be=this;
+            mainApp.get(baseUrl+'/ver', async function(req:Request, res:Response, next:NextFunction){
+                var attrs=[
+                    'browser',
+                    'version',
+                    'os',
+                    'platform',
+                ]
+                res.type('html');
+                res.write(html.h1('Datos del puesto de trabajo').toHtmlText({},{}));
+                attrs.forEach(function(attr){
+                    res.write(html.p([attr, ' ', html.b(req.useragent[attr])]).toHtmlText({},{}));
+                })
+                await be.inTransaction(req, async function(client){
+                    var ipResult = await client.query('SELECT * FROM ip WHERE ip = $1',[req.ip]).fetchOneRowIfExists();
+                    if(!ipResult.row){
+                        await client.query('INSERT INTO ip (ip) VALUES ($1)',[req.ip]).execute();
+                    }
+                    var uaResult = await client.query('SELECT * FROM user_agents WHERE ip = $1 AND user_agent = $2',[req.ip, req.headers["user-agent"]]).fetchOneRowIfExists();
+                    if(!uaResult.row){
+                        await client.query('INSERT INTO user_agents (ip, user_agent) VALUES ($1, $2)',[req.ip, req.headers["user-agent"]).execute();
+                    }
+                    console.log(req.headers);
+                    await client.query('UPDATE user_agents SET ips = $3 WHERE ip = $1 AND user_agent = $2',[req.ip, req.headers["user-agent"], req.ips]).execute();
+                    res.write('<h2>ok</h2>');
+                }).catch(err=>res.end(html.pre(err.message).toHtmlText({},{})));
+                res.end();
+            })
         }
 
         getMenu():backendPlus.MenuDefinition{
