f7e66d367de79f688a9365a0110df55a26bdb3bb
estefi-capece <eycapece@gmail.com>
2019-03-11 15:04:17 -0300

    Agrego tabla de base de datos

---
+++ src/server/table-databases.ts
@@ -0,0 +1,33 @@
+"use strict";
+
+import {TableDefinition, TableContext} from "./types-instrumentacion"
+
+export function servidores(context: TableContext): TableDefinition {
+    var admin = context.user.rol === 'admin';
+    return {
+        name: 'databases',
+        elementName: 'database',
+        editable: admin,
+        fields: [
+            { name: "database"              , typeName: 'text'    },
+            { name: "servidor"              , typeName: 'text'    },
+            { name: "port"                  , typeName: 'integer' },
+            { name: "owner"                 , typeName: 'text'    },
+            { name: "user"                  , typeName: 'text'    },
+            { name: "schema"                , typeName: 'text'    },
+            { name: "search_path"           , typeName: 'text'    },
+            { name: "sistema"               , typeName: 'text'    },
+            { name: "periodo"               , typeName: 'text'    },
+            { name: "area"                  , typeName: 'text'    },
+            { name: "estado"                , typeName: 'text'    },
+            { name: "en_campo"              , typeName: 'boolean' },
+            { name: "seg_confidencialidad"  , typeName: 'integer' },
+            { name: "seg_integridad"        , typeName: 'integer' },
+            { name: "seg_disponibilidad"    , typeName: 'integer' },
+        ],
+        primaryKey: ['servidor,database,port'],
+        foreignKeys:[
+            {references: 'servidores' , fields:['servidor'], abr:'s'/*, displayAllFields:true*/},
+        ],
+    }
+}
