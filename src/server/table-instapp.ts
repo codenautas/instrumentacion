c064c03038c75efb2acb2c61fcdd8fea2a2ed188
Sebastián <spierini@info.lifia.unlp.edu.ar>
2023-07-13 17:43:42 -0300

    se agrega orden para fecha_instalacion

--- src/server/table-instapp.ts
+++ src/server/table-instapp.ts
@@ -38,7 +38,7 @@ export function instapp(context: TableContext): TableDefinition {
             {references: 'categorias_doc' , fields:['categoria_doc']},
             {references: 'ambientes' , fields:['ambiente'], displayFields:['orden']},
         ],
-        sortColumns:[{column:'ambientes__orden', order:1}],
+        sortColumns:[{column:'ambientes__orden', order:1},{column:'fecha_instalacion', order:-1}],
         constraints:[
             {consName:'database y db_port deben especificarse simultaneamente', constraintType:'check', expr:'(database is null) = (db_port is null)'}
         ]
