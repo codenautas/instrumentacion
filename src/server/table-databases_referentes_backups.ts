f0a211cd2b0a4938c315da1b7a56216835456abc
Eugenio <eugenio.arosteguy@gmail.com>
2024-11-19 14:17:37 -0300

    @servidores#47 se agregan campos instapp.ambiente, instapp.uso, instapp.operativo y servidores.uso

--- src/server/table-databases_referentes_backups.ts
+++ src/server/table-databases_referentes_backups.ts
@@ -12,23 +12,30 @@ export function databases_referentes_backups(_context: TableContext): TableDefin
             {name: "servidor",                   typeName: "text"},
             {name: "ip",                         typeName: "text"},
             {name: "port",                       typeName: "integer"},
+            {name: "uso_servidor",               typeName: "text"},
             {name: "referentes_server",          typeName: "text"},
             {name: "responsable_backup_externo", typeName: "text"},
             {name: "instancias",                 typeName: "text"},
+            {name: "operativos_instancias",            typeName: "text"},
+            {name: "usos_instancias",            typeName: "text"},
+            {name: "ambientes_instancias",       typeName: "text"},
             {name: "repositorios",               typeName: "text"},
-            {name: "referentes_repos",            typeName: "text"},
+            {name: "referentes_repos",           typeName: "text"},
         ],
         sql:{
             isTable:false,
-            from:`(SELECT "db"."database", "db"."servidor", "s"."ip", "db"."port", "s"."referentes" "referentes_server", "s"."usuario_backups_externos" "responsable_backup_externo", 
+            from:`(SELECT "db"."database", "db"."servidor", "s"."ip", "db"."port", s.uso uso_servidor, "s"."referentes" "referentes_server", "s"."usuario_backups_externos" "responsable_backup_externo", 
                     string_agg(ia.instancia, '; ' ORDER BY ia.instancia) AS instancias, 
+                    string_agg(ia.operativo, '; ' ORDER BY ia.operativo) AS operativos_instancias,
+                    string_agg(ia.uso, '; ' ORDER BY ia.uso) AS usos_instancias, 
+                    string_agg(ia.ambiente, '; ' ORDER BY ia.ambiente) AS ambientes_instancias, 
                     string_agg(distinct "app"."repositorio", '; ' ORDER BY "app"."repositorio") AS repositorios,
                     string_agg(distinct "app"."referente", '; ' ORDER BY "app"."referente") AS referentes_repos
                 FROM databases db 
                     LEFT JOIN servidores s using(servidor) 
                     LEFT JOIN instapp ia using(database) 
                     LEFT JOIN repositorios app using(repositorio)
-                group by db.database, db.servidor, s.ip, db.port, s.referentes, s.usuario_backups_externos
+                group by db.database, db.servidor, s.ip, db.port, s.uso, s.referentes, s.usuario_backups_externos
                 ORDER BY responsable_backup_externo desc, db.servidor, referentes_repos, database)`
         },
         primaryKey: ['database','servidor','port']
